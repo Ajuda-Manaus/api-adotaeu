13fd5321bb46e3eee70edb89958667996e67d0b3
ïsaacguerreir <isaacguerreirocom@gmail.com>
2016-10-06 21:47:37 -0400

    CRUD de animais adicionado

--- routes/animais.js
+++ routes/animais.js
@@ -29,5 +29,52 @@ module.exports = (app) => {
 		  return res.json(animais);
 		});
 	});	
+
+	app.post('/animais', (req, res) => {
+    const animal = req.body;
+    animais.save(animal, (err, novoAnimal) => {
+      if (err) {
+        return res.status(412).json(err);
+      }
+      return res.json(novoAnimal);
+    });
+  });
+
+	app.get('/animais/:animalId', (req, res) => {
+    const { animalId } = req.params;
+    animais.read(animalId, (err, animal) => {
+      if (err) {
+        if (err.message === 'Invalid ID' ||
+            err.neo4jException === 'NodeNotFoundException') {
+          return res.status(404).end();
+        }
+        return res.status(412).json(err);
+      }
+      return res.json(animal);
+    });
+  });
+
+	app.put('/animais/:animalId', (req, res) => {
+    const { animalId } = req.params;
+    const animal = req.body;
+    Object.assign(animal, { id: animalId });
+    animais.save(animal, (saveErr, novoAnimal) => {
+      if (saveErr) {
+        return res.status(412).json(saveErr);
+      }
+      return res.json(novoAnimal);
+    });
+  });
+
+	app.delete('/animais/:animalId', (req, res) => {
+    const { animalId } = req.params;
+    animais.delete(animalId, (err) => {
+      if (err) {
+        return res.status(412).json(err);
+      }
+      return res.status(204).end();
+    });
+  });
+
 };
   

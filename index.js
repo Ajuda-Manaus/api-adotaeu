dd2758fcf43822894ecd57457b3eebc7d0f713cc
isaacguerreir <isaacguerreirocom@gmail.com>
2016-11-23 20:20:27 -0400

    Versão simplificada da API

--- index.js
+++ index.js
@@ -1,33 +1,24 @@
 import consign from 'consign';
 import express from 'express';
 import bodyParser from 'body-parser';
-import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import seraph from 'seraph';
 import config from './config.js';
-import session from 'express-session';
-import passport from 'passport';
 
 const app = express();
 const db = seraph(config.neo4j);
 
-app.use(cookieParser());
 app.use(bodyParser.urlencoded(config.bodyParser));
 app.use(bodyParser.json());
 app.use(compression());
 
-app.use(session({ secret: 'yoursecret' }));
-app.use(passport.initialize());
-app.use(passport.session());
 
 consign(config.consign)
   .include('models')
   .then('controlers')
   .then('routes')
-  .then('auth')
   .into(app, db)
 ;
-require('./routes/usuario.js')(app, passport);
 
 
 app.listen(config.server.port, () => {
